51534c0ba653aef202b3b96f2bcd3ea416cfcd5e
RemainingToast <Remainingtoast@gmail.com>
2022-12-15 20:15:37 +1000

    MultiPaper Master

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # MultiFabric
 > Experiment, not sure how far this will go. Check out https://multipaper.io    
 > PRs welcome.
+
+# Developing
+- Clone MultiFabric and [MultiPaper](https://multipaper.io)
+- In the MultiPaper repository, run these commands:
+  - `./gradlew :MultiPaper-Master:publishToMavenLocal`
+  - `./gradlew :MultiPaper-MasterMessagingProtocol:publishToMavenLocal`
+- Now in the MultiFabric repository run: 
+  - `./gradlew build`
--- src/main/java/io/multifabric/MultiFabric.java
+++ src/main/java/io/multifabric/MultiFabric.java
@@ -3,15 +3,20 @@ package io.multifabric;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import puregero.multipaper.server.CommandLineInput;
+import puregero.multipaper.server.MultiPaperServer;
 
 public class MultiFabric implements DedicatedServerModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final int DEFAULT_PORT = 35353;
 	public static final Logger LOGGER = LoggerFactory.getLogger("multifabric");
+	public static final MultiPaperServer MASTER = new MultiPaperServer(DEFAULT_PORT);
 
 	@Override
 	public void onInitializeServer() {
-
+		// TODO Actual Implementation of Master and Shards
+		(new CommandLineInput()).start();
 	}
 }
